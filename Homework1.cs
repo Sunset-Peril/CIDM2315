eba72a29e4ecb2c5badb5ff0b418c8e799e21836
Sunset-Peril <114113888+Sunset-Peril@users.noreply.github.com>
2023-01-23 21:26:58 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x_value = 2.5;
+        double y_value = 3.3;
+        double z_value = ((4*x_value*x_value)+(3*y_value));
+        Console.WriteLine("The value of Z is: "+z_value);
+    }
+}
+
