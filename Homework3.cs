e5af7f4841aee6904b4fb007dafa123d1a214988
Sunset-Peril <114113888+Sunset-Peril@users.noreply.github.com>
2023-02-06 00:00:09 -0600

    Add files via upload

---
+++ Homework3.cs
@@ -0,0 +1,56 @@
+﻿namespace Homework3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Question 2 must be commented out for Question 3 to work, and vise versa.
+
+        //Question 1
+        Console.WriteLine("Input an integer");
+        string input = Console.ReadLine();
+        int num = Convert.ToInt16(input);
+
+        if (num < 2)
+        {
+            Console.WriteLine("N is not prime");
+            return;
+        }
+
+        bool isPrime = true;
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime){
+            Console.WriteLine("N is prime");
+        } else {
+            Console.WriteLine("N is not prime");
+        }
+        //Question 2
+        for(int row = 0; row<5; row++){
+    for(int col = 0; col<5; col++)
+    {
+        Console.Write('#');
+    }
+    Console.WriteLine(""); 
+
+    //Question 3
+      for(int row = 0; row<5; row++){
+    for(int col = 0; col<5; col++)
+    {
+        if(row>=col)
+            Console.Write('*');
+    }
+    Console.WriteLine("");
+}
+
+    }
+}
+}
+
+
